f3b003d081166a926efc64a8e9de975703e3afa2
Batatao <babadobrazil@hotmail.com>
2018-01-04 15:32:15 +0900

    Add LevelManager + Splash Screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float timeUntilLoad = 1;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke("LoadLevelNext", timeUntilLoad);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel (string levelName)
+    {
+        Debug.Log("Level loaded : " + levelName);
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevelNext ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
