4c8d7366a8875bf67700ac6d276c6ee782260140
Echoffee <Echoffee@users.noreply.github.com>
2018-01-05 06:00:37 +0100

    Add better camera controls
    Bug when camera can be stuck on the top of the player collision box (raycast whitelist ?).

---
+++ Assets/Scripts/Gameplay/CameraManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagement : MonoBehaviour {
+
+	public GameObject Camera;
+	public float MaxDistanceCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Physics.Raycast(transform.position,  Camera.transform.position - transform.position, out hit, MaxDistanceCamera);
+		if (hit.collider != null)
+		{
+			Debug.DrawLine(transform.position, hit.point, Color.red);
+			Camera.transform.position = hit.point;
+		}
+
+		//else
+		//	Camera.transform.position = Vector3.Normalize(transform.position - Camera.transform.position) * MaxDistanceCamera * -1 + transform.position;
+	
+	}
+}
--- Assets/Scripts/Gameplay/Movement.cs
+++ Assets/Scripts/Gameplay/Movement.cs
@@ -6,9 +6,14 @@ public class Movement : MonoBehaviour {
 
 	public float Speed;
 	public float CameraSpeed;
+	public float MaxRotationSpeed;
 	public float JumpForce;
 	public float MoreGravity;
+	private bool isMoving;
+	public Transform playerModel;
+	public float CameraMaxVerticalAngleSin;
 	private bool isJumping;
+
 
 	public Rigidbody rb;
 	public Transform cameraTransform;
@@ -21,10 +26,22 @@ public class Movement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (isMoving || Mathf.Abs(Input.GetAxis("RX")) != 0)
+		{
+			float angle = Vector3.SignedAngle(playerModel.forward, characterDirection, transform.up);
+			angle = Mathf.Min(Mathf.Abs(angle), MaxRotationSpeed) * (angle >= 0 ? 1 : -1);
+			playerModel.Rotate(0, angle, 0);
+			transform.Translate((transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")) * Speed * Time.deltaTime, Space.World);
+
+		}
 		if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
 		{
+			isMoving = true;
 			characterDirection = Vector3.Normalize(transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal"));
-			transform.Translate((transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")) * Speed * Time.deltaTime, Space.World);
+		}
+		else
+		{
+			isMoving = false;
 		}
 		if (Mathf.Abs(rb.velocity.y) - 0.05f < 0)
 		{
@@ -41,7 +58,10 @@ public class Movement : MonoBehaviour {
 		{
 			rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
 		}
+		var cameraRotation = cameraTransform.rotation;
 		cameraTransform.Rotate(Input.GetAxis("RY") * transform.right * Time.deltaTime * CameraSpeed, Space.World);
+		if (Mathf.Abs(Mathf.Sin(cameraTransform.eulerAngles.x * Mathf.Deg2Rad)) > CameraMaxVerticalAngleSin )
+			cameraTransform.rotation = cameraRotation;
 		transform.Rotate(Input.GetAxis("RX") * transform.up * Time.deltaTime * CameraSpeed, Space.World);
 	}
 }
