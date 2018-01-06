6b9a6879e34e406b7c93ea8c697927c79c9a2824
Echoffee <Echoffee@users.noreply.github.com>
2018-01-06 22:59:51 +0100

    Add FX and seal purpose

---
+++ Assets/EvilScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilScript : MonoBehaviour {
+
+	private List<GameObject> seals;
+	public float Health;
+	public float Timeout;
+	private bool dead;
+	// Use this for initialization
+	void Start () {
+		seals = new List<GameObject>();
+		dead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health <= 0f && !dead)
+		{
+			dead = true;
+			foreach(var o in seals)
+				o.GetComponent<SealScript>().Kill();
+
+			foreach (var c in gameObject.GetComponentsInChildren<ParticleSystem>())
+				c.Stop();
+
+			GetComponent<BoxCollider>().enabled = false;
+		}
+
+		if (dead)
+			Timeout -= Time.deltaTime;
+	}
+
+	public void Damage(GameObject gameObject, float v)
+	{
+		if (!seals.Contains(gameObject))
+			seals.Add(gameObject);
+
+		Health -= v;
+	}
+}
--- Assets/Scripts/Gameplay/SealScript.cs
+++ Assets/Scripts/Gameplay/SealScript.cs
@@ -10,15 +10,22 @@ public class SealScript : MonoBehaviour {
 	private bool hasCollided;
 	private bool hasEffect;
 	private float alpha;
-	public Material material;
+	private bool dead;
+	public GameObject EffectOnEffective;
+	public float DamagePerSec;
+	private GameObject target;
+	public float Timeout;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		rb.AddForce(transform.forward * Speed);
+		dead = false;
+		alpha = 1;
 	}
 
-	private void OnCollisionEnter(Collision collision)
+
+	void OnCollisionEnter(Collision collision)
 	{
 		rb.velocity = Vector3.zero;
 		rb.isKinematic = false;
@@ -36,7 +43,8 @@ public class SealScript : MonoBehaviour {
 		hasEffect = collision.collider.gameObject.tag.Equals("Evil");
 		if (hasEffect)
 		{
-			//Particles or something
+			target = collision.collider.gameObject;
+			Instantiate(EffectOnEffective, transform);
 		}
 
 	}
@@ -48,21 +56,39 @@ public class SealScript : MonoBehaviour {
 
 	public void Kill()
 	{
-		alpha = 1;
+		dead = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Lifetime -= Time.deltaTime;
-		if (Lifetime <= 0f)
+		if (Lifetime <= 0f && !dead)
 			Kill();
 
 		if (hasCollided && alpha <= 0)
+		{
+			if (hasEffect)
+				foreach (var c in GetComponentsInChildren<ParticleSystem>())
+					c.Stop();
+		}
+
+		if (dead)
+			Timeout -= Time.deltaTime;
+
+		if (dead && Timeout <= 0)
 			Destroy(gameObject);
 
-		if (alpha >= 1f)
+		if (alpha > 0f && dead)
 			alpha -= Time.deltaTime;
-		material.color = new Color(material.color.r, material.color.g, material.color.b, alpha);
+
+		if (hasEffect)
+		{
+			target.GetComponent<EvilScript>().Damage(gameObject, DamagePerSec * Time.deltaTime);
+		}
+
+		Renderer rend = GetComponent<Renderer>();
+		var material = rend.material;
+		rend.material.SetColor("_Color", new Color(material.color.r, material.color.g, material.color.b, alpha));
 		
 	}
 }
