c797844ce5ea54c5506633ee7165b7126eaf9261
ahaeflig <adan.haefliger@gmail.com>
2018-01-06 08:17:16 +0100

    Improve bamboo scene using some naturalstarterkit2 asset

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController instance = null;
+
+    private int Xbox_One_Controller = 0;
+    private int PS4_Controller = 0;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    private void Start()
+    {
+        string[] names = Input.GetJoystickNames();
+        for (int x = 0; x < names.Length; x++)
+        {
+            //print(names[x].Length);
+            if (names[x].Length == 19)
+            {
+                print("PS4 CONTROLLER IS CONNECTED");
+                PS4_Controller = 1;
+                Xbox_One_Controller = 0;
+            }
+            if (names[x].Length == 33)
+            {
+                print("XBOX ONE CONTROLLER IS CONNECTED");
+                //set a controller bool to true
+                PS4_Controller = 0;
+                Xbox_One_Controller = 1;
+            }
+        }
+    }
+
+    public bool IsPS4Controller()
+    {
+        return PS4_Controller == 1;
+    }
+
+    public bool IsXBOXONEController()
+    {
+        return Xbox_One_Controller == 1;
+    }
+
+
+
+}
