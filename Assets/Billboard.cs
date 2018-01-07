c8512410a86392eacb22bf50e41fc5134985cd1a
ahaeflig <adan.haefliger@gmail.com>
2018-01-07 18:57:12 +0100

    billboard and ui message

---
+++ Assets/Billboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    public GameObject text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+
+}
