3493eb4c7355a5197d693b59f36ee2d72d0c8878
Echoffee <Echoffee@users.noreply.github.com>
2018-01-07 21:45:29 +0100

    Fix static MechaChilds

--- Assets/Scripts/MechaChild.cs
+++ Assets/Scripts/MechaChild.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MechaChild : MonoBehaviour, IMechanism{
+
+	private IMechanism parentMechanism;
+
 	public bool Activate(MechanismInfo info)
 	{
-		return transform.parent.GetComponent<IMechanism>().Activate(info);
+		if (parentMechanism != null)
+			return parentMechanism.Activate(info);
+		else
+			return true;
 	}
 
 	public Vector3 GetGluedValue()
 	{
-		return transform.parent.GetComponent<IMechanism>().GetGluedValue();
+		if (parentMechanism != null)
+			return parentMechanism.GetGluedValue();
+		else
+			return Vector3.zero;
 
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+		parentMechanism = transform.parent.GetComponent<IMechanism>();
 	}
 	
 	// Update is called once per frame
