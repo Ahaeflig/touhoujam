7a23385f8b89eca05e46739dd800218bd20cddf8
ahaeflig <adan.haefliger@gmail.com>
2018-01-07 09:39:26 +0100

    add killzone

---
+++ Assets/Killzone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Killzone : MonoBehaviour {
+
+    public GameObject gameOverUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //TODO desactivate player controls
+            gameOverUI.SetActive(true);
+            MusicPlayer.instance.setSong(3);
+        }
+    }
+
+}
