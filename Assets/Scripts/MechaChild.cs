7ebfeed2f6a9b584781243d2575bd7b49790c8a0
Echoffee <Echoffee@users.noreply.github.com>
2018-01-07 21:41:33 +0100

    Fix meshed hitboxes

---
+++ Assets/Scripts/MechaChild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechaChild : MonoBehaviour, IMechanism{
+	public bool Activate(MechanismInfo info)
+	{
+		return transform.parent.GetComponent<IMechanism>().Activate(info);
+	}
+
+	public Vector3 GetGluedValue()
+	{
+		return transform.parent.GetComponent<IMechanism>().GetGluedValue();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
